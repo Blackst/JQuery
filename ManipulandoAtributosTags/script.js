e187fda6e6ddafcc561e7a4762d850601bb2076d
Thiago Mendes <tm.thiagom86@gmail.com>
2019-07-10 12:16:27 -0300

    Manipulando Atributos e Tags

---
+++ ManipulandoAtributosTags/script.js
@@ -0,0 +1,23 @@
+$(function(){
+
+	//Atributo para colocar link na tag a do html.
+
+	$('a').attr('href', 'http://www.google.com.br')
+});
+
+$(function(){
+
+	//Atributo que modifica o tamanho da imagem.
+	$('img').attr('width', '200')
+});
+
+$(function(){
+
+	$('img').attr('border', '5')
+});
+
+$(function(){
+
+	//Troca para outra imagem.
+	//$('img').attr('src', 'imagem.jpg')
+});
